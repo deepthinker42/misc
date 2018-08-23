960c3c36c555832eff24e8b5339e4a59280e4ee2
Robert Goodman <robert.goodman@amd.com>
2018-08-23 11:41:38 -0500

    More modifications

--- jenkinsinfo.py
+++ jenkinsinfo.py
@@ -7,6 +7,7 @@ import json
 import traceback
 import argparse
 import datetime
+import time
 if sys.version >= (3,):
     from urllib.parse import urlencode
     from urllib.request import urlopen, Request
@@ -22,11 +23,15 @@ COMPUTER = '/computer'
 
 class JenkinsInfo(object):
 
-    def __init__(self, host, port, username, password):
-        self.host = host
-        self.port = port
-        self.username = username
-        self.password = password
+    def __init__(self, args):
+        self.host = args.host
+        self.port = args.port
+        self.username = args.username
+        self.password = args.password
+        self.days = args.days
+        self.search = args.search
+        self.job = args.job
+        self.build = args.build
         self.server = None
         return
 
@@ -54,56 +59,41 @@ class JenkinsInfo(object):
                     key = [job['url'], job['color'].replace('_anime', '')]
                     if key not in active_jobs:
                         active_jobs.append(key)
-        return active_jobs
+        return sorted(active_jobs)
 
-    def getBuilds(self, job_names, hours_back, text=None):
-        if text:
-            print('Getting the list of builds in the past %d hours with "%s" in the console output.  This will take 5-10 minutes.' % (hours_back, text))
+    def getBuilds(self, job_names, days_back, search_for=None):
+        if search_for:
+            print('Getting the list of builds in the past %d days with "%s" in the console output.  This will take a while.' % (days_back, search_for))
         else:
-            print('Getting the list of builds in the past %d hours.  This will take around 2-5 minutes.' % hours_back)
+            print('Getting the list of builds in the past %d days.  This will take a while.' % days_back)
         builds = []
-        ts = datetime.datetime.now() - datetime.timedelta(hours=hours_back)
+        num_builds = 0
+        time_start = time.time()
+        ts = datetime.datetime.now() - datetime.timedelta(days=days_back)
         ts = int(ts.strftime('%s'))
-        for job_name in job_names:
-            url = 'http://%s:%d/job/%s%s' % (self.host, self.port, job_name, API_JSON)
+        if self.job:
+            job_names = [self.job]
+        for i, job_name in enumerate(job_names):
+            url = 'http://%s:%d/job/%s%s?tree=allBuilds[url,timestamp]' % (self.host, self.port, job_name, API_JSON)
             try:
-                response = urlopen(url, timeout=5)
+                response = urlopen(url, timeout=120)
                 data = json.loads(response.read())
-                if not 'builds' in data:
+                if not 'allBuilds' in data:
                     continue
-                for build in data['builds']:
-                    url2 = build['url'] if not build['url'].endswith('/') else build['url'][:-1]
-                    url2 += API_JSON
-                    try:
-                        response2 = urlopen(url2, timeout=5)
-                        data2 = json.loads(response2.read())
-                        if not 'timestamp' in data2:
-                            continue
-                        ts2 = int(data2['timestamp']) // 1000
-                        if ts2 < ts:
-                            break
-                        if text:
-                            url3 = url2.replace(API_JSON, '/console')
-                            try:
-                                response3 = urlopen(url3, timeout=5)
-                                data3 = response3.read()
-                                if not text in data3:
-                                    continue
-                            except Exception as e3:
-                                #print('ERROR: could not open "%s": %s\n%s' % (url3, e3, traceback.format_exc(e3)))
-                                #print('ERROR: could not open "%s"' % url3)
-                                continue
-                            builds.append(url3)
-                        else:
-                            builds.append(url2)
-                    except Exception as e2:
-                        #print('ERROR: could not open "%s": %s\n%s' % (url2, e2, traceback.format_exc(e2)))
-                        #print('ERROR: could not open "%s"' % url2)
-                        continue
+                use_builds = []
+                for build in data['allBuilds']:
+                    ts2 = int(build['timestamp']) // 1000
+                    if ts2 < ts:
+                        break
+                    use_builds.append([url, ts2])
+                print('        %s (%d%%)  (%d of %d)' % (job_name, (i * 100)/len(job_names), len(use_builds), len(data['allBuilds'])))
+                builds.extend(use_builds)
             except Exception as e:
-                #print('ERROR: could not open "%s": %s\n%s' % (url, e, traceback.format_exc(e)))
-                #print('ERROR: could not open "%s"' % url)
+                #print('ERROR 3: could not open "%s": %s\n%s' % (url, e, traceback.format_exc(e)))
                 continue
+        time_end = time.time()
+        time_total = int(time_end - time_start)
+        print('Time to get the list of %d builds: %d min %d sec' % (len(builds), time_total // 60, time_total % 60))
         return builds
 
     def getComputersInLabel(self):
@@ -138,7 +128,7 @@ class JenkinsInfo(object):
             data = self.server.get_info(entry['url'])
             for job in data['jobs']:
                 job_names.append('%s' % job['name'])
-        return job_names
+        return sorted(job_names)
 
     def main(self):
         self.server = self.connect()
@@ -152,9 +142,40 @@ class JenkinsInfo(object):
         self.showComputersInLabel(computers_in_label)
         self.showActiveJobs(active_jobs)
         """
-        builds = self.getBuilds(job_names, 24, 'remoting')
-        self.showBuilds(builds, 24, 'remoting')
+        builds = self.getBuilds(job_names, self.days)
+        self.processBuilds(builds, self.search)
+        self.showBuilds(builds, self.days, self.search)
         return True
+
+    def processBuilds(self, builds, search_for):
+        time_start = time.time()
+        for build in builds:
+            url = build[0]
+            ts = build[1]
+            if search_for:
+                console_url = url.replace(API_JSON, '/console')
+                failed_hosts = []
+                try:
+                    response = urlopen(url, timeout=15)
+                    lines = response.readlines()
+                    for line in lines:
+                        if not search_for in line:
+                            continue
+                        if 'Cannot contact' in line:
+                            failed_hosts.setdefault(line.split('contact ', 1)[1].split(':', 1)[0], True)
+                except Exception as e:
+                    print('ERROR: could not open "%s": %s\n%s' % (url3, e3, traceback.format_exc(e3)))
+                    continue
+                if failed_hosts:
+                    failed_hosts = sorted(failed_hosts.keys())
+                    i = 0
+                    while i < len(failed_hosts):
+                        print('        ' + '%s' % ', '.join(failed_hosts[i:i+12]))
+                        i += 12
+        time_end = time.time()
+        time_total = int(time_end - time_start)
+        print('Time to process %d builds: %d min %d sec' % (len(builds), time_total // 60, time_total % 60))
+        return
 
     def showActiveJobs(self, active_jobs):
         print('\n------------------------')
@@ -170,11 +191,11 @@ class JenkinsInfo(object):
             print('There are no active jobs')
         return
 
-    def showBuilds(self, builds, hours_back, text=None):
-        if text:
-            print('Builds in the last %d hours with "%s" in the console output:' % (hours_back, text))
+    def showBuilds(self, builds, days_back, search_for=None):
+        if search_for:
+            print('Builds in the last %d days with "%s" in the console output:' % (days_back, search_for))
         else:
-            print('Builds in the last %d hours:' % hours_back)
+            print('Builds in the last %d days:' % days_back)
         for url in builds:
             print(url)
         return
@@ -207,8 +228,12 @@ if __name__ == '__main__':
     parser.add_argument('-p', '--port', type=int, help='Jenkins master port')
     parser.add_argument('-u', '--username', help='Jenkins master login username')
     parser.add_argument('-P', '--password', help='Jenkins master login password')
+    parser.add_argument('-d', '--days', type=int, help='Days in the past to look at (default: %(default)d day(s))', default=1)
+    parser.add_argument('-s', '--search', help='String to search for in the build logs', default=None)
+    parser.add_argument('-j', '--job', help='Only search a specific job', default=None)
+    parser.add_argument('-b', '--build', help='Only search a specific build', default=None)
     args = parser.parse_args()
-    JI = JenkinsInfo(args.host, args.port, args.username, args.password)
+    JI = JenkinsInfo(args)
     if not JI.main():
         exit(1)
 
