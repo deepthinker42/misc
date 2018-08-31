8c1abfa89e1e19e784c5ccd7ccd5f78567eb7487
Robert Goodman <robert.goodman@amd.com>
2018-08-31 10:50:24 -0500

    Updated memory hoisting

--- automation/testcase/memory_hoisting/memory_hoisting.py
+++ automation/testcase/memory_hoisting/memory_hoisting.py
@@ -6,7 +6,10 @@ import argparse
 import subprocess
 
 
-
+NINE_FFFF = 0x9ffff
+A0000 = 0xa0000
+FFFFF = 0xfffff
+ONE_MB = 0x100000
 FOUR_GB = 4 * 1024 * 1024 * 1024
 ONE_TB = 1024 * 1024 * 1024 * 1024
 
@@ -36,8 +39,10 @@ class MemoryHoistingTest(object):
             try:
                 cmd = 'ssh %s@%s dmesg > %s' % (self.username, self.sut, dmesg_out)
                 subprocess.call(cmd, shell=True)
+                """
                 cmd = 'ssh %s@%s free -gt > %s' % (self.username, self.sut, free_out)
                 subprocess.call(cmd, shell=True)
+                """
             except Exception as e:
                 print('Error: command "%s" failed: %s\n%s' % cmd, e, traceback.format_exc(e))
                 return False
@@ -49,20 +54,24 @@ class MemoryHoistingTest(object):
                 return False
             self.dmesg_file = dmesg_out
             self.free_file = free_out
-        elif not os.path.exists(self.dmesg_file):
+        if not os.path.exists(self.dmesg_file):
             print('Error: "%s" does not exist' % self.dmesg_file)
             return False
-        elif not os.path.exists(self.free_file):
+        """
+        if not os.path.exists(self.free_file):
             print('Error: "%s" does not exist' % self.free_file)
             return False
 
         with open(self.free_file) as f:
             free_lines = f.readlines()
+        """
         with open(self.dmesg_file) as f:
             dmesg_lines = f.readlines()
+        """
         self.test_free(free_lines)
+        """
         self.test_bios_e820(dmesg_lines)
-        self.test_srat_node_0_pxm_0(dmesg_lines)
+        self.test_srat_node(dmesg_lines)
         return True
 
     def process_dmesg(self):
@@ -71,7 +80,7 @@ class MemoryHoistingTest(object):
     def test_free(self, lines):
         free_total_memory = 0
         for line in lines:
-            if line.startswith('Mem:'):
+            if 'Mem:' in line:
                 free_total_memory = int(line.split()[1])
                 break
         if abs(self.total_memory - free_total_memory) >= 3:
@@ -85,55 +94,112 @@ class MemoryHoistingTest(object):
         for line in lines:
             if not 'BIOS-e820: ' in line:
                 continue
-            cols = line.strip().split()
-            starting = int(cols[1], 16)
-            ending = int(cols[3], 16)
-            label = cols[4].strip('(').strip(')')
+            if '[mem' in line:
+                starting, ending, label = line.strip().split('[mem ')[1].replace('-', ' ').replace(']', '').split(' ', 2)
+            else:
+                starting, ending, label = line.strip().split('e820: ')[1].replace('-', ' ').split(' ', 2)
+                label = label.replace('(', '').replace(')', '').strip()
+            starting = int(starting, 16)
+            ending = int(ending, 16)
             self.bios_e820_addr_dict.setdefault(label, {}).setdefault(starting, ending)
             self.bios_e820_addr_list.extend([starting, ending])
         self.bios_e820_addr_list = sorted(self.bios_e820_addr_list)
-        for i in list(range(len(self.bios_e820_addr_list) - 1)):
-            if self.bios_e820_addr_list[i] > self.bios_e820_addr_list[i+1]:
-                print('FAIL:  BIOS-e820:  addresses are not contiguous: %s' % ' '.join([hex(x) for x in self.bios_e820_addr_list]))
-                return False
-        print('PASS:  BIOS-e820:  addresses are contiguous')
-        if self.total_memory > 4:
-            found = False
-            for k, v in self.bios_e820_addr_dict['reserved'].items():
-                if k < FOUR_GB and v <= (FOUR_GB * .95):
-                    found = True
-            if not found:
-                print('FAIL:  BIOS-e820:  no memory range found within 95%% of 4GB')
-                return False
-            print('PASS:  BIOS-e820:  memory range found within 95% of 4GB')
+
+        """
+        if not self.test_bios_e820_contiguous():
+            return False
+        """
+        if not self.test_bios_e820_0_to_9ffff_usable():
+            return False
+        if not self.test_bios_e820_a0000_to_fffff_reserved():
+            return False
+        if not self.test_bios_e820_100000_to_100000000_usable():
+            return False
         return True
 
-    def test_srat_node_0_pxm_0(self, lines):
+    def test_bios_e820_contiguous(self):
+        for i in list(range(2, len(self.bios_e820_addr_list), 2)):
+            if self.bios_e820_addr_list[i] - 1 != self.bios_e820_addr_list[i - 1]:
+                print('FAIL:  BIOS-e820:  addresses are not contiguous: 0x%x, 0x%x' % (self.bios_e820_addr_list[i - 1], self.bios_e820_addr_list[i]))
+                return False
+        print('PASS:  BIOS-e820:  addresses are contiguous')
+        return True
+
+    def test_bios_e820_0_to_9ffff_usable(self):
+        if not 0 in self.bios_e820_addr_dict['usable'] or self.bios_e820_addr_dict['usable'][0] != NINE_FFFF:
+            print('FAIL:  BIOS-e820:  address range 0x0 - 0x9ffff is not usable')
+            return False
+        print('PASS:  BIOS-e820:  address range 0x0 - 0x9ffff is usable')
+        return True
+
+    def test_bios_e820_a0000_to_fffff_reserved(self):
+        if not A0000 in self.bios_e820_addr_dict['reserved'] or self.bios_e820_addr_dict['reserved'][A0000] != FFFFF:
+            print('FAIL:  BIOS-e820:  address range 0xa0000 - 0xfffff is not reserved')
+            return False
+        print('PASS:  BIOS-e820:  address range 0xa0000 - 0xfffff is reserved')
+        return True
+
+    def test_bios_e820_100000_to_100000000_usable(self):
+        if not ONE_MB in self.bios_e820_addr_dict['usable']:
+            print('FAIL:  BIOS-e820:  address range starting with 0x100000 is not usable')
+            return False
+        print('PASS:  BIOS-e820:  address range starting with 0x100000 is usable')
+        return True
+
+    def test_bios_e820_4GB_usable(self):
+        if not FOUR_GB in self.bios_e820_addr_dict['usable']:
+            print('FAIL:  BIOS-e820:  address range starting with 0x100000000 is not usable')
+            return False
+        print('PASS:  BIOS-e820:  address range starting with 0x100000000 is usable')
+        return True
+
+    def test_srat_node(self, lines):
         addr_list = []
         addr_dict = {}
         for line in lines:
             if not 'SRAT: Node ' in line:
                 continue
-            cols = line.strip().split()[-1].split('-')
-            starting = int(cols[0], 16)
-            ending = int(cols[1], 16)
+            cols = line.strip().split()[-1].replace(']', '').split('-')
+            starting = long(cols[0], 16)
+            ending = long(cols[1], 16)
             addr_list.extend([starting, ending])
             addr_dict[starting] = ending
-        if not FOUR_GB in addr_dict:
-            print('FAIL:  SRAT:  no address range starting at 4GB')
-        else:
-            print('PASS:  SRAT:  usable address starting at 4GB')
-            if not addr_dict[FOUR_GB] == self.bios_e820_addr_dict['usable'][FOUR_GB]:
-                print('FAIL:  SRAT:  no matching address range starting at 4GB found in BIOS-e820')
-            else:
-                print('PASS:  SRAT:  matching address range starting at 4GB found in BIOS-e820')
-        if self.total_memory >= 1024:
-            if not ONE_TB in addr_dict:
-                print('FAIL:  SRAT:  no address range starting at 1TB found')
-            else:
-                print('PASS:  SRAT:  address range starting at 1TB found')
-        else:
-            print('PASS:  SRAT:  total memory < 1TB')
+
+        if not self.test_srat_node_0_to_under_4GB_contiguous(addr_dict):
+            return False
+        if not self.test_srat_node_memory_hole_under_4GB_hoisted_to_4GB(addr_dict):
+            return False
+        if not self.test_srat_node_memory_hole_under_1TB_hoisted_to_1TB(addr_list, addr_dict):
+            return False
+        return True
+
+    def test_srat_node_0_to_under_4GB_contiguous(self, addr_dict):
+        starting_addr_indices = list(range(2, len(self.bios_e820_addr_list), 2))
+        for starting_addr in starting_addr_indices:
+            if (self.bios_e820_addr_list[starting_addr] != self.bios_e820_addr_list[starting_addr - 1] + 1) and self.bios_e820_addr_list[starting_addr] < addr_dict[0]:
+                print('FAIL:  SRAT:  address ranges between 0 and 0x100000000 are not contiguous (0x%0x, 0x%0x)' % (self.bios_e820_addr_list[starting_addr - 1], self.bios_e820_addr_list[starting_addr]))
+                break
+        print('PASS:  SRAT:  address ranges between 0 and 0x100000000 are contiguous')
+        return True
+
+    def test_srat_node_memory_hole_under_4GB_hoisted_to_4GB(self, addr_dict):
+        hole_size = (FOUR_GB - 1) - (addr_dict[0] + 1)
+        if addr_dict[FOUR_GB] < FOUR_GB + hole_size:
+            print('FAIL:  SRAT:  0x%x hole starting 0x%x not hoisted above 4GB (0x%x)' % (hole_size, addr_dict[0] + 1, addr_dict[FOUR_GB]))
+            return False
+        print('PASS:  SRAT:  0x%x hole starting 0x%x hoisted above 4GB (0x%x - 0x%x)' % (hole_size, addr_dict[0] + 1, FOUR_GB, addr_dict[FOUR_GB]))
+        return True
+
+    def test_srat_node_memory_hole_under_1TB_hoisted_to_1TB(self, addr_list, addr_dict):
+        if not ONE_TB in addr_dict:
+            print('PASS:  SRAT:  no hole at 1TB because system has less than 1TB of memory')
+            return True
+        ending_addr = addr_list[addr_list.index(ONE_TB) - 1]
+        hole_size = (ONE_TB - 1) - (ending_addr + 1)
+        if addr_dict[ONE_TB] < ONE_TB + hole_size:
+            print('FAIL:  SRAT:  0x%x hole starting 0x%x not hoisted above 1TB (0x%x)' % (hole_size, ending_addr + 1, addr_dict[ONE_TB]))
+            return False
+        print('PASS:  SRAT:  0x%x hole starting 0x%x hoisted above 1TB (0x%x - 0x%x)' % (hole_size, ending_addr + 1, ONE_TB, addr_dict[ONE_TB]))
         return True
 
 
