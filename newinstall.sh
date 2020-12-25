343418a867fb1e95306ad6852d91703c911d0c8c
Robert Goodman <deepthinker42@gmail.com>
2020-12-25 17:26:59 -0600

    Update newinstall.sh

--- newinstall.sh
+++ newinstall.sh
@@ -25,4 +25,16 @@ sudo dnf install -y golang
 sudo dnf install -y npm
 sudo dnf install -y kubernetes ansible kubernetes-ansible kubernetes-kubeadm
 
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+cat << EOF | sudo tee /etc/yum.repos.d/vscode.repo
+[code]
+name=Visual Studio Code
+baseurl=https://packages.microsoft.com/yumrepos/vscode
+enabled=1
+gpgcheck=1
+gpgkey=https://packages.microsoft.com/keys/microsoft.asc
+EOF
+sudo dnf check-update
+sudo dnf install -y code
+
 #
