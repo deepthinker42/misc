0e3774cad17cfaa8b98dd01c5476fd05ab2a089f
Robert Goodman <deepthinker42@gmail.com>
2020-12-25 15:54:49 -0600

    Update newinstall.sh

--- newinstall.sh
+++ newinstall.sh
@@ -6,3 +6,7 @@ wget https://github.com/balena-io/etcher/releases/download/v1.5.113/balena-etche
 unzip balena-etcher-electron-1.5.113-linux-ia32.zip
 chmod 755 balena-etcher-electron-1.5.113-linux-ia32.zip
 ./balena-etcher-electron-1.5.113-x64.AppImage
+
+sudo dnf install -y fedora-workstation-repositories
+sudo dnf config-manager --set-enabled google-chrome
+sudo dnf install -y google-chrome-stable
